90f9e5c2eef9aff1ac97d68fe3c2c4173d0aa5ac
marsnebulasoup <marsnebulasoup@gmail.com>
2023-01-30 11:10:18 -0600

    Updated exhibit to allow children

--- sextant/src/components/Exhibit/index.js
+++ sextant/src/components/Exhibit/index.js
@@ -1,12 +1,14 @@
 import './styles.css'
 
-export function Exhibit() {
+export function Exhibit(props) {
   return (
     <div class="exhibit-container">
       <div class="exhibit-header">
         <span class="exhibit-header-title">Dashboard</span>
       </div>
-      <div class="exhibit">Exhibit Container</div>
+      <div class="exhibit-background">
+        <div class="exhibit">{props.children}</div>
+      </div>
     </div>
   );
 }
