a0233105398352326082e8d8a638b72c2712e79b
marsnebulasoup <marsnebulasoup@gmail.com>
2023-01-30 09:15:06 -0600

    Added banner component

---
+++ sextant/src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export function Banner() {
+  return (
+    <div class="banner-container">
+      <div class="banner">
+        <span class="banner-title">Sextant</span>
+      </div>
+    </div>
+  );
+}
