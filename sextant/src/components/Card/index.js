f69c12bb95661ce2f9e0cd372713fc44b189f24a
marsnebulasoup <marsnebulasoup@gmail.com>
2023-01-30 12:00:04 -0600

    Made card stateless

--- sextant/src/components/Card/index.js
+++ sextant/src/components/Card/index.js
@@ -1,16 +1,10 @@
 import "./styles.css";
-import { useState } from 'react';
-
 
 export function Card(props) {
-  const [content, setContent] = useState("Loading...");
-
-  props.content().then(setContent);
-
   return (
     <div class="card">
       <div class="card-title">{props.title}</div>
-      <div class="card-content">{content}</div>
+      <div class="card-content">{props.content}</div>
     </div>
   );
 }
